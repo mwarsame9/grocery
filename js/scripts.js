080fa788976c0c13aa70caaf2ba87af7e0bae292
Mohamed Warsame and Nick Powell <mohamed@>
2017-03-27 10:55:58 -0700

    add console log

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   $("#favForm").submit(function(event) {
+
     var movieInput = $("input#movie").val();
     var iceCreamInput = $("input#iceCream").val();
     var colorInput = $("input#color").val();
     var carInput = $("input#car").val();
     var drinkInput = $("input#drink").val();
     var placeInput = $("input#place").val();
+    var favorites = [movieInput, iceCreamInput, colorInput, carInput, drinkInput, placeInput]
 
-    $(".movie").text(movieInput);
-    $(".iceCream").text(iceCreamInput);
-    $(".color").text(colorInput);
-    $(".car").text(carInput);
-    $(".drink").text(drinkInput);
-    $(".place").text(placeInput);
+    console.log(favorites[2],favorites[0],favorites[3]);
 
 
-    $("#result").show();
+
+    // $("#result").show()
+
+
 
     event.preventDefault();
 
